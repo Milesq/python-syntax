11623e5da3a1a826029e4b9b90cbd5a012545f53
Milesq <wmilosz88@gmail.com>
2019-11-04 12:33:33 +0100

    Add indent function

--- src/main/kotlin/pythonSyntax.kt
+++ src/main/kotlin/pythonSyntax.kt
@@ -1,9 +1,41 @@
 package com.gmail.wmilosz88.pythonSyntax
 
-fun insert(code: String): String = code
+import java.io.File
+
+fun insert(code: String): String = code.split('\n').map {
+    var transformed = code
+    // if (it[0] == "#") code
+
+    transformed
+}.joinToString("\n")
+
+fun String.indent(): Int {
+    var indent = 0
+    for (letter in this) {
+        if (letter.isWhitespace())
+            indent++
+        else
+            break
+    }
+
+    return indent
+}
 
 fun pythonSyntax(code: String, insertSemi: Boolean = true): String {
-    var transformCode: String = if (insertSemi) insert(code) else code
+    var transformCode = code.split('\n')
+        .filter { it.length != 0 }
+        .map {
+            if (it[it.lastIndex] == ':') {
+                println(it.indent())
+                it.slice(0..it.length-2) + "{"
+            } else {
+                it
+            }
+        }
 
-    return code
+    // println(transformCode.joinToString("\n"))
+    // transformCode: String = if (insertSemi) insert(transformCode) else transformCode
+
+    return " "
+    // return transformCode
 }
